be2c681eb7e72398da796b77f6c5ba879115ea72
GAURAV SHARMA <sgaurav607@gmail.com>
2021-04-04 23:28:23 +0530

    GHAR KA KHANNA

---
+++ app/src/main/java/com/example/recipeapp/FoodData.java
@@ -0,0 +1,46 @@
+package com.example.recipeapp;
+
+public class FoodData {
+
+    private String itemName;
+    private String itemDescription;
+    private String itemPrice;
+    private String itemImage;
+    private String key;
+
+
+    public FoodData(String recipename, String recipeDescription, String recipePrice, String imageUrl) {
+    }
+
+    public FoodData(String itemName, String itemDescription, String itemPrice, String itemImage, String phone) {
+        this.itemName = itemName;
+        this.itemDescription = itemDescription;
+        this.itemPrice = itemPrice;
+        this.itemImage = itemImage;
+
+    }
+
+    public String getItemName() {
+        return itemName;
+    }
+
+    public String getItemDescription() {
+        return itemDescription;
+    }
+
+    public String getItemPrice() {
+        return itemPrice;
+    }
+
+    public String getItemImage() {
+        return itemImage;
+    }
+
+    public String getKey() {
+        return key;
+    }
+
+    public void setKey(String key) {
+        this.key = key;
+    }
+}
